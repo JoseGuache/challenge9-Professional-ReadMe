28545629ef867c56e967d8d21c24a9a12c6ce239
James Guache <jamesg2126@gmail.com>
2025-02-10 02:17:49 -0500

    Added the missing questions to the questionaire that are required to meet the acceptance criteria. Also added more licenses to the question that was provided w the starter code. Because of this, I also added the extra license links to the generateMarkdown.js file.

--- index.js
+++ index.js
@@ -1,36 +1,74 @@
 // TODO: Include packages needed for this application
-const fs=require("fs")
-const generateMarkdown=require("./utils/generateMarkdown")
-const inquirer=require("inquirer")
+const fs = require("fs")
+const generateMarkdown = require("./utils/generateMarkdown")
+const inquirer = require("inquirer")
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type:"input",
-        message:"What is the title of your project",
-        name:"title",
-    }
-    ,
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
     {
-        type:"list",
-        message:"Choose the following license:",
-        name:"license",
-        choices:["MIT","ISC"]
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+        validate: function (value) {
+            const valid = /\S+@\S+\.\S+/;
+            return valid.test(value) || 'Sorry the email is not valid. Please try again.'
+        }
+    },
+    {
+        type: "input",
+        message: "Enter a description of the project",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "What is the usage information?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "What are the contribution guidelines?",
+        name: "contributing",
+    },
+    {
+        type: "input",
+        message: "What are the test instructions?",
+        name: "tests",
+    },
+    {
+        type: "list",
+        message: "Choose the following license:",
+        name: "license",
+        choices: ["MIT", "ISC", "Apache 2.0", "Boost 1.0", "EPL 1.0", "MPL 2.0"]
     }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  const content=generateMarkdown(data)
-  fs.writeFile(fileName,content,(err)=>err?console.error(err):console.log("success"))
+    const content = generateMarkdown(data)
+    fs.writeFile(fileName, content, (err) => err ? console.error(err) : console.log("success"))
 }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-    .prompt(questions)
-    .then(data=>{
-        writeToFile("./output/README.md",data)
-    })
+        .prompt(questions)
+        .then(data => {
+            writeToFile("./output/README.md", data)
+        })
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,4 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === "MIT") {
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
@@ -22,30 +21,42 @@ function renderLicenseBadge(license) {
 }
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  const licenseLinks = {
+    "MIT": "https://opensource.org/licenses/MIT",
+    "ISC": "https://opensource.org/licenses/ISC",
+    "Apache 2.0": "https://opensource.org/licenses/Apache-2.0",
+    "Boost 1.0": "https://www.boost.org/LICENSE_1_0.txt",
+    "EPL 1.0": "https://opensource.org/licenses/EPL-1.0",
+    "MPL 2.0": "https://opensource.org/licenses/MPL-2.0"
+  };
+  return licenseLinks[license] || '';
+}
 
 // TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  if (!license) return '';
+  return `This project is licensed under the ${license} license. For more information, visit ${renderLicenseLink(license)}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-${renderLicenseBadge(data.license)}  
+${renderLicenseBadge(data.license)}
 
 ## Description
 
- ${data.description}
+${data.description}
 
-## Table of Contents (Optional)
-
+## Table of Contents
 
 - [Installation](#installation)
 - [Usage](#usage)
-- [Credits](#credits)
 - [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
 ## Installation
 
@@ -53,43 +64,26 @@ ${data.installation}
 
 ## Usage
 
-${data.usage} 
-    
-
-## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
+${data.usage}
 
 ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+${renderLicenseSection(data.license)}
 
----
+## Contributing
 
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-## Features
-
-If your project has a lot of features, list them here.
-
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+${data.contributing}
 
 ## Tests
 
+${data.tests}
 
+## Questions
 
+For additional questions, you can reach me through:
+
+GitHub: [${data.github}](https://github.com/${data.github})
+Email: ${data.email}
 `;
 }
 
